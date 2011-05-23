79070e43abc7a73b73563eff56c7d1cf127a714e
Christopher Poole <christopher.poole@qut.edu.au>
2011-05-24 09:21:53 +1000

    added setup.py for easy_install / pypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='manysim',
+      version='0.0,1',
+      description='Perform Simulations on the Amazon Elastic Compute Cloud',
+      long_description='manysim is a simple utility for executing arbitary \
+                        code on EC2 using Python and the boto Python module. \
+                        Jobs can be launched from the local user machine and \
+                        executed on EC2 with results returned to the user via S3',
+      author='Christopher M Poole',
+      author_email='mail@christopherpoole.net',
+      url='http://code.google.com/p/manysim/',
+      py_modules=['manysim'],
+      data_files=[('example', ['example/job.py', 'example/launch.py']),
+                  ('scripts', ['scripts/geant4', 'scripts/run.py'])],
+      install_requires=['boto'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: Apache Software License',],
+     )
+     
